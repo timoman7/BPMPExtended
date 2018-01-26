416730f2b7c3d36bd4684dd1bec54b40996b5d22
Tim C <tc20791@sestudent.org>
2018-01-26 16:54:00 -0500

    a

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,19 +5,35 @@ const loadingIcon = document.getElementById('loading');     //Span
 const result = document.getElementById("result");           //Span
 const SteamResult = document.getElementById('SteamResult'); //Div
 const Retry = document.getElementById('Retry');             //Button
+const myHeaders = new Headers();
+const myConf = {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default'
+};
+
 
 function CheckBan(SID){
   BanInput.classList.replace('visible','hidden');
   loadingIcon.classList.replace('hidden','visible');
 
-  return fetch(`https://marketplace.tf/api/Bans/GetUserBan/v1?steamid=${SID}`)
+  return fetch(`https://marketplace.tf/api/Bans/GetUserBan/v1?steamid=${SID}`, myConf)
 }
 
 function CBClick(){
   CheckBan(SteamID64.value).then((verdict)=>{
     loadingIcon.classList.replace('visible','hidden');
     result.classList.replace('hidden','visible');
+    window.verdict = verdict;
+    console.log(verdict.json())
   })
 }
 
+function RetryClick(){
+  result.classList.replace('visible','hidden');
+  BanInput.classList.replace('hidden','visible');
+}
+
 Check_Ban.addEventListener('click', CBClick);
+Retry.addEventListener('click', RetryClick);
